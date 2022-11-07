4c201987e63843c06ac4929013c9284fd3f33abe
Picasso <urvishp13@gmail.com>
2022-11-07 18:56:27 -0500

    got hardcoded buttons to appear on screen

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,9 +1,26 @@
-// get the link to do API requests
+// Get the link to do API requests
 var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8MdAEnC5o8xrtEnYfGNqV3fkqDjgoK62";
 
+// Hard code categories and give them the formatting I want all buttons to have
+var categories = ['batman','wonder woman','superman','aquaman','green lantern','captain america','iron man'];
+
+// Loop through each category and add their buttons to #searched-categories. Give each category a .category class
+categories.forEach(category => {
+        
+    // Create button
+    let $categoryButton = $('<button>')
+                        // Give each button the same class
+                        .attr('class', 'category')
+                        // Add text to the button of the current category
+                        .text(category);
+    
+    // Add newly created button to #searched-categories
+    $('#searched-categories').append($categoryButton);
+});
+
 // One AJAX call
-    // extract searched category text from input field once button is clicked
-    // additionally, add searched category to #searched-categories list
+    // Extract searched category text from input field once #search-button is clicked
+    // Additionally, add searched category to #searched-categories array
     
 // Another AJAX call
-    // show 10 pictures of ANY clicked category from #searched-categories in #images field
+    // Show 10 pictures of ANY clicked category from #searched-categories in #images field
