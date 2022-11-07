aa8486c4cd7078803ad87902ae026f50b599eb9d
Picasso <urvishp13@gmail.com>
2022-11-07 17:46:06 -0500

    extracted js from index.html and placed in app.js file

---
+++ assets/javascript/app.js
@@ -0,0 +1,9 @@
+// get the link to do API requests
+var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8MdAEnC5o8xrtEnYfGNqV3fkqDjgoK62";
+
+// One AJAX call
+    // extract searched category text from input field once button is clicked
+    // additionally, add searched category to #searched-categories list
+    
+// Another AJAX call
+    // show 10 pictures of ANY clicked category from #searched-categories in #images field
