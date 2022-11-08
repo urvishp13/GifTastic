70de2389fbc7c960247d5dd649865202989a16b0
Picasso <urvishp13@gmail.com>
2022-11-07 19:19:08 -0500

    was able to successfully add a superhero entry from input field to section with all the superhero names

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,26 +1,36 @@
+function createButton(superhero) {
+    // Create button
+    let $superheroButton = $('<button>')
+                        // Give each button the same class
+                        .attr('class', 'superhero')
+                        // Add text to the button of the current superhero
+                        .text(superhero);
+    
+    // Add newly created button to #searched-superheroes
+    $('#superheroes').append($superheroButton);
+}
+
 // Get the link to do API requests
 var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=8MdAEnC5o8xrtEnYfGNqV3fkqDjgoK62";
 
-// Hard code categories and give them the formatting I want all buttons to have
-var categories = ['batman','wonder woman','superman','aquaman','green lantern','captain america','iron man'];
+// Hard code superheroes and give them the formatting I want all buttons to have
+var superheroes = ['batman','wonder woman','superman','aquaman','green lantern','captain america','iron man'];
 
-// Loop through each category and add their buttons to #searched-categories. Give each category a .category class
-categories.forEach(category => {
-        
-    // Create button
-    let $categoryButton = $('<button>')
-                        // Give each button the same class
-                        .attr('class', 'category')
-                        // Add text to the button of the current category
-                        .text(category);
-    
-    // Add newly created button to #searched-categories
-    $('#searched-categories').append($categoryButton);
-});
+// Loop through each superhero and add their buttons to #searched-superhero. Give each superhero a .superhero class
+superheroes.forEach(superhero => createButton(superhero));
 
-// One AJAX call
-    // Extract searched category text from input field once #search-button is clicked
-    // Additionally, add searched category to #searched-categories array
+// When the add superhero button is clicked
+$('#add-superhero').click(function () {
+    // One AJAX call
+    $.ajax({
+        url: "index.html"
+    })
+        // Extract searched superhero text from input field once #search-button is clicked
+        .then(function () {
+            let newSuperhero = $('#search-field').val();
+            createButton(newSuperhero);
+        })
+    });
     
 // Another AJAX call
-    // Show 10 pictures of ANY clicked category from #searched-categories in #images field
+    // Show 10 pictures of ANY clicked superhero from #searched-superheroes in #images field
